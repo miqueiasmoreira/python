046908b43f1417c75ac5344800a1ebfd9d61ae0c
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-08-16 22:38:40 -0300

    u

--- Curso Python/desafios/be.py
+++ Curso Python/desafios/be.py
@@ -1,2 +1,7 @@
-R = float(input())
-print(f"VOLUME = {(4/3.0) * 3.14159 * (R**3):.3}")
+A, B, C = map(int, input().split())
+
+maior_ab = (A+B+abs(A-B)) / 2
+
+maior_abc = (maior_ab + C + abs(maior_ab - C)) // 2
+
+print(f"{maior_abc} eh o maior")
