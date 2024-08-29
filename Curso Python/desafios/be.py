1c1af0f12c7b50341e22a97f658a92122a79f4e8
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-08-28 21:41:00 -0300

    Update

--- Curso Python/desafios/be.py
+++ Curso Python/desafios/be.py
@@ -1,7 +1,2 @@
-A, B, C = map(int, input().split())
-
-maior_ab = (A+B+abs(A-B)) / 2
-
-maior_abc = (maior_ab + C + abs(maior_ab - C)) // 2
-
-print(f"{maior_abc} eh o maior")
+n1 = int(input())
+print(f'{n1 * 24}')
---
+++ Curso Python/desafios/desafio019.py
@@ -0,0 +1,7 @@
+import random
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+print(f'{random.choice(lista)}')
