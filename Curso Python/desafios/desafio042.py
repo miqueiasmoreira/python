4a21b5e283f20dc8c087980c99a9cf915bc4b011
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-09-19 23:14:00 -0300

    Update

--- Curso Python/desafios/desafio042.py
+++ Curso Python/desafios/desafio042.py
@@ -1,15 +1,18 @@
+from time import time
 print("-=-" * 20)
 print("Analisador de triângulos")
 print("-=-" * 20)
 r1 = float(input("Primeiro segmento: "))
 r2 = float(input("Segundo segmento: "))
 r3 = float(input("Terceiro segmento: "))
-segmento = (r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2)
-if (r1 == r2 and r3 and segmento == True):
-    print("EQUILÁTERO, todos os lados iguais")
-elif (r1 == r2 or r1 == r3 or r2 == r3 and segmento == True):
-    print("ISÓSCELES, dois lados iguais")
-elif (r1 != r2 or r1 != r3 or r2 != r3 and segmento == True):
-    print("ESCALENO, todos os lados diferentes")
+if (r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2):
+    print("Os seguimentos acima PODEM FORMAR um Triângulo", end=' ')
+    if (r1 == r2 == r3):
+        print("EQUILÁTERO!")
+    elif (r1 != r2 != r3 != r1):
+        print("ESCALENO!")
+    else:
+        print("ISÓSCELES")
 else:
-    print("Nenhum formou um triangulo")
+    print("Os seguimentos acima NÃO PODEM formar Triângulo")
+print("---FIM---")
--- Curso Python/desafios/desafio044.py
+++ Curso Python/desafios/desafio044.py
@@ -1,6 +1,25 @@
-price = float(input("Valor do produto: "))
-if (price > 0):
-    print(f"À vista (Dinheiro ou Cheque): {} com 10% de desconto.")
-    print(f"À vista (Cartão): {} com 5% de desconto.")
-    print(f"2 vezes (Cartão: {} sem juros")
-    print(f"3 vezes ou mais (Cartão {} 20% de juros)")
+price = float(input("Preço das compras: R$"))
+print("FORMAS DE PAGAMENTO")
+print("[1] À vista (Dinheiro ou Cheque) com 10% de desconto.")
+print("[2] À vista (Cartão) com 5% de desconto.")
+print("[3] 2x (Cartão) sem juros")
+print("[4] 3x ou mais (Cartão) 20% de juros")
+price_option = float(input("Qual é a opção de pagamento? "))
+if (price_option == 1):
+    print(f"Sua compra de R${price:.2f} vai custar no final R${price - (price * 0.1):.2f}")
+elif (price_option == 2):    
+    print(f"Sua compra de R${price:.2f} vai custar no final R${price - (price * 0.05):.2f}")
+elif (price_option == 3):    
+    print(f"Sua compra de R${price:.2f} vai custar no final 2 parcelas de R${price / 2:.2f}")
+elif (price_option == 4):
+    qtd_parcela = int(input("Quantas parcelas?"))
+    print(f"Sua compra será parcelada em {qtd_parcela}x de R${(price + price * 0.2) / qtd_parcela:.2f} com JUROS")
+    print(f"Sua compra de R${price} vai custar R${(price + price * 0.2):.2f} no final")
+else:
+    print("ERRO")
+    
+    
+    
+    
+    
+    
