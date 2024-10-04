a862408c715f523c936d5a6986ba17370083d948
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-10-03 21:30:21 -0300

    Update

---
+++ Curso Python/exercicios/ex21.py
@@ -0,0 +1,16 @@
+'''for c in range(1, 10):
+    print(c)
+print('FIM')'''
+
+'''c = 1
+
+while c < 10:
+    print(c)
+    c += 1
+print('FIM')'''
+
+'''number = 2
+while number > 1:
+    number = int(input('Number: '))
+print('Finalmente digitou o valor correto')'''
+
