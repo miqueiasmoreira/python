ccdae21b09384442cfa743f2cc976505d4087422
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-10-15 21:49:22 -0300

    Update desafio065.py

--- Curso Python/desafios/desafio065.py
+++ Curso Python/desafios/desafio065.py
@@ -4,7 +4,8 @@ Quer continuar?[S/N]
 
 Você digitou {} números e a média foi {}
 O maior valor foi {} e o menor foi {}'''
-n = cont = soma = 0
+
+'''n = cont = soma = 0
 n = int(input('Digite um número: '))
 
 while n != 'Nn':
@@ -15,4 +16,23 @@ while n != 'Nn':
         n = int(input('Digite um número: '))
     else:
         print(f'Você digitou {cont} números e a média foi {soma / cont:.2f}')
-        break
+        break'''
+        
+resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: ').strip().upper())
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+           maior = num
+        if num < menor:
+            menor = num 
+    resp = str(input('Que continuar? [S/N] ').strip().upper()[0])
+media = soma / quant
+print(f'Você digitou {quant} números e a mpedia foi {media:.2f}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
+    
