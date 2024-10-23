29c206871e13e914949dafac01522acaa7458200
Miquéias Moreira <89392888+miqueiasmoreira@users.noreply.github.com>
2024-10-22 23:03:48 -0300

    Update

--- Curso Python/desafios/desafio067.py
+++ Curso Python/desafios/desafio067.py
@@ -1,22 +1,16 @@
-n = s = 0
-while True:
-    n = int(input('Digite um número: '))
-    if n == 999:
-        break
-    s += n
-print(f'A soma vale {s}')
+'''num = int(input("Digite um número: ").strip())
 
-
+for c in range(1, 10+1):
+    print(f'{num} x {c} = {num * c}')
+print('FIM')'''
 
 n = cont = 0
 while True:
     cont = 0
     n = int(input('Digite um valor para ver a tabuada: '))
-    while cont < 11:
-        if n < 0:
-            break
-        print(f'{n} x {cont} = {n * cont}')
-        cont += 1
     if n < 0:
         break
+    while cont < 11:
+        print(f'{n} x {cont} = {n * cont}')
+        cont += 1
 print('Programa interrompido')
