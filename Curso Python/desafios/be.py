b56ddd6c7d00a6e6175bb54d41d6a9f214fecebb
Miquéias Moreira <miqueiasmoreirasouza@gmail.com>
2024-12-22 18:07:46 -0300

    Update be.py

--- Curso Python/desafios/be.py
+++ Curso Python/desafios/be.py
@@ -1,7 +1,23 @@
-valores = list()
-for cont in range(0, 5):
-    valores.append(int(input('Digite um valor: ')))
+def calcular_notas(valor):
+    # Lista de valores das notas disponíveis
+    notas = [100, 50, 20, 10, 5, 2, 1]
+    resultado = []
 
-for c, v in enumerate(valores):
-    print(f'Na posição {c} encontrei o valor {v}!')
-print('Cheguei ao final da lista.')
+    for nota in notas:
+        quantidade = valor // nota  # Calcula quantas notas são necessárias
+        resultado.append(quantidade)  # Armazena o resultado
+        valor %= nota  # Atualiza o valor restante
+
+    return resultado
+
+def exibir_resultado(valor, notas):
+    print(valor)
+    valores_notas = [100, 50, 20, 10, 5, 2, 1]
+
+    for quantidade, nota in zip(notas, valores_notas):
+        print(f"{quantidade} nota(s) de R$ {nota},00")
+
+# Leitura do valor de entrada
+valor = int(input())
+notas_calculadas = calcular_notas(valor)
+exibir_resultado(valor, notas_calculadas)
